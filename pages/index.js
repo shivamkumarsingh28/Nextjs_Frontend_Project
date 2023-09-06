7ac958f39aaccecdc89817ce52263153a3515143
shivamkumarsingh28 <110661272+shivamkumarsingh28@users.noreply.github.com>
2023-09-06 22:59:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Hero from '../components/Hero';
-import Section1 from '../components/section1';
+import { Section1 } from '../components/section1';
 import { Section2 } from '../components/Section2';
 import { Section3 } from '../components/Section3';
 import { Section4 } from '../components/Section4';
