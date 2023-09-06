675308575db6b559278a5f357559fd339c6992ad
shivamkumarsingh28 <110661272+shivamkumarsingh28@users.noreply.github.com>
2023-09-06 22:40:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import Hero from '../components/Hero';
-import Section1  from 'components/section1';
-import Section2 from '../components/Section2';
-import { Section3 } from '../components/Section3';
-import { Section4 } from '../components/Section4';
-import { Section5 } from '../components/Section5';
-import { Section6 } from '../components/Section6';
-import { Section7 } from '../components/Section7';
+import { Section1 } from '@/components/section1';
+import { Section2 } from '@/components/Section2';
+import { Section3 } from '@/components/Section3';
+import { Section4 } from '@/components/Section4';
+import { Section5 } from '@/components/Section5';
+import { Section6 } from '@/components/Section6';
+import { Section7 } from '@/components/Section7';
 
 
 export default function Home() {
